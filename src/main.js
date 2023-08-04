510d7e62be920fdc3d93c77b18a0c85c8e50bcb3
WahabAhmad22 <99469759+WahabAhmad22@users.noreply.github.com>
2023-08-04 18:15:35 +0500

    Follow up

--- src/main.js
+++ src/main.js
@@ -1,3 +1,50 @@
 import './styles/style.css'
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+console.log(OrbitControls)
 
-console.log('Hello')
+const canvas = document.querySelector("canvas");
+// Scene
+const scene = new THREE.Scene()
+
+// Box
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const materials = new THREE.MeshBasicMaterial({color: "yellow"});
+const box = new THREE.Mesh(geometry, materials)
+scene.add(box)
+
+// Sizes
+const size = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+
+    size.width = window.innerWidth
+    size.height = window.innerHeight
+
+    camera.aspect = size.width / size.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(size.width, size.height)
+    renderer.render(scene, camera )
+
+})
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight)
+camera.position.z = 5
+scene.add(camera)
+
+// Orbit Control
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera )
+
